8e8aec3292796af78551df3305b0fbff165622e7
LeKAKiD <lekakid@kakao.com>
2021-02-03 19:12:32 +0900

    feat: 스프라이트 맵핑 테이블 생성 추가

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -1,10 +1,36 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Sirenix.OdinInspector;
 
 public class BoardController : MonoBehaviour
 {
+    [BoxGroup("Item")]
+    public Transform ItemContainer;
+
+    [BoxGroup("Item"), AssetsOnly]
+    public GameObject ItemPrefab;
+
+    struct MappingItem {
+        public SpriteRenderer spriteRenderer;
+        public Transform transform;
+        public bool exists;
+    }
+    MappingItem[,] _MappingTable;
+
+    private void Awake() {
+        _MappingTable = new MappingItem[10, 20];
+        for(int y = 0; y < 20; y++) {
+            for(int x = 0; x < 10; x++) {
+                GameObject item = Instantiate(ItemPrefab, ItemContainer.localPosition + new Vector3(x, y, 0), Quaternion.identity);
+                item.transform.SetParent(ItemContainer);
+                _MappingTable[x, y].spriteRenderer = item.GetComponent<SpriteRenderer>();
+                _MappingTable[x, y].transform = item.transform;
+            }
+        }
+    }
+
     public void OnMove(InputAction.CallbackContext context) {
         if(context.performed) {
             Debug.Log("십자키 입력 시작");
