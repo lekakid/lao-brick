9ea4aed19c86526cba39ee22a8fd420af0a587dd
LeKAKiD <lekakid@kakao.com>
2021-02-03 18:54:50 +0900

    feat: 블록 랜덤 생성 코드 추가

---
+++ Assets/Scripts/BrickGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class BrickGenerator : SerializedMonoBehaviour
+{
+    [TableList(DrawScrollView = false)]
+    public BrickScriptableObject[] DB;
+    Queue<BrickScriptableObject> bagQueue = new Queue<BrickScriptableObject>();
+    
+    public BrickScriptableObject GetRandomBrick() {
+        if(bagQueue.Count < 1) {
+            List<BrickScriptableObject> TempArr = new List<BrickScriptableObject>(DB);
+            while(TempArr.Count > 0) {
+                int r = Random.Range(0, TempArr.Count);
+                bagQueue.Enqueue(TempArr[r]);
+                TempArr.RemoveAt(r);
+            }
+        }
+        return bagQueue.Dequeue();
+    }
+}
