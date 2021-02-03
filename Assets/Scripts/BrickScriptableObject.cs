f5c31687824a19e2fc3597b356042583046ca962
LeKAKiD <lekakid@kakao.com>
2021-02-03 19:16:57 +0900

    feat: 블록 데이터 스크립트 추가

---
+++ Assets/Scripts/BrickScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[InlineEditor]
+public class BrickScriptableObject : SerializedScriptableObject
+{
+    [TableColumnWidth(57, false), PreviewField]
+    public Sprite Preview;
+    public Sprite[] Blocks;
+    public Vector2[][] Offsets = new Vector2[][] { new Vector2[4], new Vector2[4], new Vector2[4], new Vector2[4]};
+}
