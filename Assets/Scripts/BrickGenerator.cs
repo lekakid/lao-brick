60fba5247ac40218c7d37a50ce6b4991f8adf157
LeKAKiD <lekakid@kakao.com>
2021-02-04 09:19:35 +0900

    refactor: 무작위 블록 생성 코드 분리

--- Assets/Scripts/BrickGenerator.cs
+++ Assets/Scripts/BrickGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
@@ -8,16 +8,18 @@ public class BrickGenerator : SerializedMonoBehaviour
     [TableList(DrawScrollView = false)]
     public BrickScriptableObject[] DB;
     Queue<BrickScriptableObject> bagQueue = new Queue<BrickScriptableObject>();
+
+    void GenerateBag() {
+        List<BrickScriptableObject> TempArr = new List<BrickScriptableObject>(DB);
+        while(TempArr.Count > 0) {
+            int r = Random.Range(0, TempArr.Count);
+            bagQueue.Enqueue(TempArr[r]);
+            TempArr.RemoveAt(r);
+        }
+    }
     
     public BrickScriptableObject GetRandomBrick() {
-        if(bagQueue.Count < 1) {
-            List<BrickScriptableObject> TempArr = new List<BrickScriptableObject>(DB);
-            while(TempArr.Count > 0) {
-                int r = Random.Range(0, TempArr.Count);
-                bagQueue.Enqueue(TempArr[r]);
-                TempArr.RemoveAt(r);
-            }
-        }
+        if(bagQueue.Count < 1) GenerateBag();
         return bagQueue.Dequeue();
     }
 }
