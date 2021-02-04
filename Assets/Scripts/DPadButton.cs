a48b7948a337bd4a8915807f38757076777bf340
LeKAKiD <lekakid@kakao.com>
2021-02-04 21:16:37 +0900

    feat: OnScreenButton 대체할 UI 클래스 추가

---
+++ Assets/Scripts/DPadButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DPadButton : Selectable
+{
+    [System.Serializable]
+    public class DirectionEvent : UnityEvent<Vector2> {}
+
+    [SerializeField]
+    Vector2 direction;
+
+    public DirectionEvent OnButtonDown;
+    public UnityEvent OnButtonUp;
+
+    public override void OnPointerDown(PointerEventData eventData) {
+        base.OnPointerDown(eventData);
+        OnButtonDown.Invoke(direction);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData) {
+        base.OnPointerUp(eventData);
+        OnButtonUp.Invoke();
+    }
+}
