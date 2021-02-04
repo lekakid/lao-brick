6d79b040d83fd9deba9b8dca43a9dba23bc7e0d1
LeKAKiD <lekakid@kakao.com>
2021-02-04 20:38:25 +0900

    feat: 타이틀 화면 구현

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour
+{
+    [SerializeField]
+    BoardController BoardController;
+    [SerializeField]
+    Animator TitleGroupAnimator;
+
+    public void OnClickStart() {
+        BoardController.StartGame();
+        TitleGroupAnimator.SetTrigger("out");
+    }
+}
