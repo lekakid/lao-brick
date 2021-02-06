9c5c98d29166a9ababf1ac0c92c364be9223c0c9
LeKAKiD <lekakid@kakao.com>
2021-02-06 17:38:35 +0900

    feat: 배경 구름 구현

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    private void FixedUpdate() {
+        transform.Translate(Vector2.left * Time.fixedDeltaTime);
+        if(transform.position.x < -15f) {
+            transform.Translate(Vector3.right * 30f);
+        }
+    }
+}
