169917250558d551f51da8849c43bc5b05f22c14
LeKAKiD <lekakid@kakao.com>
2021-02-15 00:01:11 +0900

    feat: 타이틀 BGM 재생

--- Assets/Scripts/TitleController.cs
+++ Assets/Scripts/TitleController.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class TitleController : MonoBehaviour
 {
     [SerializeField]
+    AudioMixerController AudioMixerController;
+    [SerializeField]
     BoardController BoardController;
     [SerializeField]
     Animator TitleGroupAnimator;
+
+    private void Start() {
+        AudioMixerController.PlayBGM("Title");
+    }
 
     public void OnClickStart() {
         BoardController.StartGame();
