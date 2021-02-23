9636d793a0ac811784935e8220f45a5d2b2ff090
LeKAKiD <lekakid@kakao.com>
2021-02-24 00:19:37 +0900

    refactor: Title 애니메이터 직관성 개선

--- Assets/Scripts/TitleController.cs
+++ Assets/Scripts/TitleController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +9,7 @@ public class TitleController : MonoBehaviour
     [SerializeField]
     BoardController BoardController;
     [SerializeField]
-    Animator TitleGroupAnimator;
+    Animator TitleAnimator;
     [SerializeField]
     ModeController ModeController;
     [SerializeField]
@@ -17,10 +17,25 @@ public class TitleController : MonoBehaviour
 
     private void Start() {
         AudioMixerController.PlayBGM("Title");
+        Show();
+    }
+
+    public void Show() {
+        TitleAnimator.SetBool("Toggle", true);
+        isFocus = true;
+    }
+
+    public void Hide() {
+        TitleAnimator.SetBool("Toggle", false);
+        isFocus= false;
+    }
+
+    private void Start() {
+        AudioMixerController.PlayBGM("Title");
     }
 
     public void OnClickStart() {
-        TitleGroupAnimator.SetBool("Toggle", true);
+        Hide();
         ModeController.Show();
     }
 
