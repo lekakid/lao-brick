5bde5c5286df8c18c49965d9afa095c061429917
lekakid <lekakid@kakao.com>
2021-03-24 17:22:16 +0900

    feat: GameEvent 시스템 구현

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvent : ScriptableObject
+{
+    List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void RegiesterListener(GameEventListener listener) {
+        listeners.Add(listener);
+    }
+
+    public void UnregisterListener(GameEventListener listener) {
+        listeners.Remove(listener);
+    }
+
+    public void Invoke() {
+        if(listeners.Count == 0) return;
+
+        for(int i = 0; i < listeners.Count; i++) {
+            listeners[i].OnEventRaised();
+        }
+    }
+}
---
+++ Assets/Scripts/GameEventListener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent GameEvent;
+    public UnityEvent Callbacks;
+
+    private void OnEnable() {
+        GameEvent.RegiesterListener(this);
+    }
+
+    private void OnDisable() {
+        GameEvent.UnregisterListener(this);
+    }
+
+    public void OnEventRaised() {
+        Callbacks.Invoke();
+    }
+}
