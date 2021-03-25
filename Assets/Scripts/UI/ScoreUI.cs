8a6510f5739b9878c8010bf7b978b5647ee2a1a9
lekakid <lekakid@kakao.com>
2021-03-25 21:02:46 +0900

    feat: 스코어 UI 컴포넌트 추가

---
+++ Assets/Scripts/UI/ScoreUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+    public GameDataSO GameData;
+
+    public Text ScoreText;
+    public Text HighScoreText;
+
+    public void UpdateScore() {
+        ScoreText.text = string.Format("{0,8:D8}", GameData.Score);
+        HighScoreText.text = string.Format("{0,8:D8}", GameData.HighScore);
+    }
+}
